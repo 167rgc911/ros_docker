4af04ab536e8e23dba930698b7cdcf17d80b9499
Rommel Custodio <sk0rpsting@protonmail.ch>
2020-11-25 09:19:47 +0000

    set privileged

--- foxy.sh
+++ foxy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #docker run -it --rm my/ros:foxy /ros_entrypoint.sh /bin/bash
-docker run -it -v /althome:/althome --rm my/ros:foxy /ros_entrypoint.sh /bin/bash
+docker run -it --privileged -v /althome:/althome --rm my/ros:foxy /ros_entrypoint.sh /bin/bash
